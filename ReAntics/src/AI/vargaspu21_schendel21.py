5e526902a340a6e6cf1bc720160f24764150cde0
Lexden12 <lexden.s@gmail.com>
2020-02-22 11:54:50 -0800

    Test verified MiniMax, speedups included

--- ReAntics/src/AI/vargaspu21_schendel21.py
+++ ReAntics/src/AI/vargaspu21_schendel21.py
@@ -5,6 +5,7 @@ from Ant import UNIT_STATS
 from Construction import CONSTR_STATS
 from Constants import *
 from Player import *
+from AgentTest import *
 import random
 import sys
 sys.path.append("..")  # so other modules can be found in parent dir
@@ -32,6 +33,13 @@ class AIPlayer(Player):
         super(AIPlayer, self).__init__(inputPlayerId, "MiniMax")
         self.root = None
         self.prune = True
+        self.test = False
+
+    def init2(self):
+      self.test = True
+      self.winCount = -1
+      self.moveCount = -1
+      self.me = 0
 
     ##
     #getPlacement
@@ -48,25 +56,8 @@ class AIPlayer(Player):
     #Return: The coordinates of where the construction is to be placed
     ##
     def getPlacement(self, currentState):
-        numToPlace = 0
-        #implemented by students to return their next move
-        if currentState.phase == SETUP_PHASE_1:  # stuff on my side
-            numToPlace = 11
-            moves = []
-            for i in range(0, numToPlace):
-                move = None
-                while move == None:
-                    #Choose any x location
-                    x = random.randint(0, 9)
-                    #Choose any y location on your side of the board
-                    y = random.randint(0, 3)
-                    #Set the move if this space is empty
-                    if currentState.board[x][y].constr == None and (x, y) not in moves:
-                        move = (x, y)
-                        #Just need to make the space non-empty. So I threw whatever I felt like in there.
-                        currentState.board[x][y].constr == True
-                moves.append(move)
-            return moves
+        if currentState.phase == SETUP_PHASE_1:
+            return [(3, 1), (7, 1), (0, 3), (1, 3), (2, 3), (3, 3), (5, 3), (6, 3), (7, 3), (8, 3), (9, 3)]
         elif currentState.phase == SETUP_PHASE_2:  # stuff on foe's side
             numToPlace = 2
             moves = []
@@ -99,28 +90,30 @@ class AIPlayer(Player):
     def getMove(self, currentState):
         self.me = currentState.whoseTurn
         if self.root is None:
-          self.root = Node(None, currentState, 0, None)
+          self.root = Node(None, currentState, 0, None, self.test)
 
           # For loops goes until set depth
           # in this case a depth of 3 
           self.total = 0
           self.expandNode(self.root)
           print(self.total)
-        maxNode = self.root.children[0]
-        maxScore = self.root.children[0].score
-        for child in self.root.children:
-          #print(child.move)
-          #print(child.score)
-          if child.score > maxScore:
-            maxScore = child.score
-            maxNode = child
-        print(maxNode.score)
-        print(maxNode.move)
-        if maxNode.move.moveType == END or self.root.children == []:
-          self.root = None
-        else:
-          self.root = maxNode
-        return maxNode.move
+        if not len(self.root.children) == 0:
+          maxNode = self.root.children[0]
+          maxScore = self.root.children[0].score
+          for child in self.root.children:
+            #print(child.move)
+            #print(child.score)
+            if child.score > maxScore:
+              maxScore = child.score
+              maxNode = child
+          print(maxNode.score)
+          print(maxNode.move)
+          if maxNode.move.moveType == END or self.root.children == []:
+            self.root = None
+          else:
+            self.root = maxNode
+          return maxNode.move
+        return Move(END)
 
     def legalMove(self, state, move):
       if move.coordList is not None and move.moveType == MOVE_ANT:
@@ -138,34 +131,55 @@ class AIPlayer(Player):
     # returns a list of nodes
     ##
     def expandNode(self, node):
-      win = getWinner(node.state)
+      win = None
+      if not self.test:
+        win = getWinner(node.state)
+      else:
+        win = getWinnerMock()
       if win is not None:
         node.score = 100 if (win == 1) else -100
         node.score = node.score if (node.turn == self.me) else -node.score
         if node.parent is not None:
           node.parent.updateBounds(node, self.me)
         return
-      if node.depth == 4:
-        node.score = self.utility(node.state)
+      if node.depth == 3:
+        if not self.test:
+          node.score = self.utility(node.state)
+        else:
+          node.score = utilityMock()
         if node.parent is not None:
           node.parent.updateBounds(node, self.me)
         return
-      moves = listAllLegalMoves(node.state)
+      if not self.test:
+        moves = listAllLegalMoves(node.state)
+      else:
+        moves = listMovesMock()
       for move in moves:
-        nextState = getNextStateAdversarial(node.state, move)
+        if not self.test:
+          nextState = getNextStateAdversarial(node.state, move)
+        else:
+          nextState = None
+        if node.depth > 0 and node.move.moveType == BUILD and not move.moveType == END:
+          continue
         newDepth = node.depth + 1
-        newNode = Node(move, nextState, newDepth, node)
+        newNode = Node(move, nextState, newDepth, node, self.test)
+        if self.test and newDepth % 2 == 0:
+          newNode.setTurn(0)
+        if self.test and not newDepth % 2 == 0:
+          newNode.setTurn(1)
         node.addChild(newNode)
         self.expandNode(newNode)
         if self.prune and node.parent is not None:
-          if node.turn == self.me:#Must be on previous iteration
+          if node.turn == self.me:
             if node.alpha >= node.parent.beta or (node.score is not None and node.score >= node.parent.beta):
-#             print("Alpha")
+              if self.test:
+                print("Alpha: {}, {}".format(node.alpha, node.parent.beta))
               node.parent.removeChild(node)
               return
           else:
             if node.beta <= node.parent.alpha or (node.score is not None and node.score <= node.parent.alpha):
-#             print("Beta")
+              if self.test:
+                print("Beta: {}, {}".format(node.beta, node.parent.alpha))
               node.parent.removeChild(node)
               return
       
@@ -243,10 +257,10 @@ class AIPlayer(Player):
 
       # value army size
       if me == self.me:
-        myScore += min(len(myOffense), 3) * 10
+        myScore += min(len(myOffense), 3) * 15
         enemyScore += max((min(len(enemyDrones), 5) * 7), min(len(enemyOffense), 3) * 15)
       else:
-        enemyScore += min(len(myOffense), 3) * 10
+        enemyScore += min(len(myOffense), 3) * 15
         myScore += max((min(len(enemyDrones), 5) * 7), min(len(enemyOffense), 3) * 15)
 
       # encourage more food gathering
@@ -270,60 +284,61 @@ class AIPlayer(Player):
       myScore += 21 - 7*(enemyHill.captureHealth)
       dist = 100
       for ant in enemyOffense:
-        dist = stepsToReach(currentState, ant.coords, myHill.coords)
+        dist = approxDist(ant.coords, myHill.coords)
         enemyScore += 10 - min(dist, 10)
       enemyScore += 21 - 7*(myHill.captureHealth)
 
       # Gather food
-#     if me == self.me:
-#       workers = myWorkers
-#       tunnel = myTunnel
-#       hill = myHill
-#     else:
-#       workers = enemyWorkers
-#       tunnel = enemyTunnel
-#       hill = enemyHill
-#     score = 0
-#     for w in workers:
-#       if w.carrying: # if carrying go to hill/tunnel
-#         score += 3
-#         distanceToTunnel = stepsToReach(currentState, w.coords, tunnel.coords)
-#         distanceToHill = stepsToReach(currentState, w.coords, hill.coords)
-#         dist = min(distanceToHill, distanceToTunnel)
-#         if dist > 2 and dist <= 4:
-#           score += 1
-#         if dist <= 2:
-#           score += 2
-#       else: # if not carrying go to food
-#         dist = 100
-#         for food in foods:
-#           if stepsToReach(currentState, w.coords, food.coords) < dist:
-#             dist = stepsToReach(currentState, w.coords, food.coords)
-#         if dist > 2 and dist <= 4:
-#           score += 1
-#         if dist <= 2:
-#           score += 2
-#     if self.me == me:
-#       myScore += score
-#     else:
-#       enemyScore += score
-      for w in myWorkers:
-        if w.carrying:
-          myScore += 4
-      myScore += myFood * 7
+      if me == self.me:
+        workers = myWorkers
+        tunnel = myTunnel
+        hill = myHill
+      else:
+        workers = enemyWorkers
+        tunnel = enemyTunnel
+        hill = enemyHill
+      score = 0
+      for w in workers:
+        if w.carrying: # if carrying go to hill/tunnel
+          score += 3
+          distanceToTunnel = approxDist(w.coords, tunnel.coords)
+          distanceToHill = approxDist(w.coords, hill.coords)
+          dist = min(distanceToHill, distanceToTunnel)
+          if dist > 2 and dist <= 4:
+            score += 1
+          if dist <= 2:
+            score += 2
+        else: # if not carrying go to food
+          dist = 100
+          for food in foods:
+            temp = approxDist(w.coords, food.coords)
+            if temp < dist:
+              dist = temp
+          if dist > 2 and dist <= 4:
+            score += 1
+          if dist <= 2:
+            score += 2
+      if self.me == me:
+        myScore += score
+      else:
+        enemyScore += score
+#     for w in myWorkers:
+#       if w.carrying:
+#         myScore += 4
+#     myScore += myFood * 7
 
-#     if self.me == me:
-#       workers = enemyWorkers
-#     else:
-#       workers = myWorkers
-#     score = 0
-      for w in enemyWorkers:
+      if self.me == me:
+        workers = enemyWorkers
+      else:
+        workers = myWorkers
+      score = 0
+      for w in workers:
         if w.carrying:
           enemyScore += 4
-#     if self.me == me:
-#       enemyScore += score
-#     else:
-#       myScore += score
+      if self.me == me:
+        enemyScore += score
+      else:
+        myScore += score
       enemyScore += enemyFood * 7
 
       score = min(myScore - enemyScore, 95) if (myScore - enemyScore > 0) else max(myScore - enemyScore, -95)
@@ -332,6 +347,8 @@ class AIPlayer(Player):
         score = 100 if (win == 1) else -100
       return score if (me == self.me) else -score
 
+     
+
 ##
 # Node Class
 #
@@ -339,16 +356,22 @@ class AIPlayer(Player):
 #
 ##
 class Node:
-  def __init__(self, move, state, depth, parent):
+  def __init__(self, move, state, depth, parent, test):
     self.move = move
     self.state = state
     self.depth = depth
     self.score = None
-    self.turn = state.whoseTurn
+    if not test:
+      self.turn = state.whoseTurn
+    else:
+      self.turn = None
     self.alpha = -200
     self.beta = 200#alpha and beta values for minimax
     self.children = []
     self.parent = parent
+  
+  def init2():
+    self.test = True
 
   def addChild(self, node):
     self.children.append(node)
@@ -363,3 +386,6 @@ class Node:
     else:
       self.beta = min(self.beta, node.score)
     return True
+
+  def setTurn(self, turn):
+    self.turn = turn
